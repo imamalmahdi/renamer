937038c516052e9ec6bc522df93360add67aeeac
mahdi <mahdihossain987@gmail.com>
2020-12-17 19:56:31 +0600

    added a new auto-mode

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 from datetime import datetime
+import json
 
 CURRENT_DIR = Path(__file__).parent.absolute()
 LOG_FILENAME = f"{CURRENT_DIR}/logs/" + datetime.now().strftime('%H_%M_%S_%d_%m_%Y.log')
 CACHE_FILE = Path(f"{CURRENT_DIR}/cached_hash.txt")
+
+def load_setting():
+    settings_file = Path(f"{CURRENT_DIR}/settings.json")
+    return json.loads(settings_file.read_text())
--- renamer.py
+++ renamer.py
@@ -1,7 +1,17 @@
 from directories import Directory
+from sys import argv
+from config import load_setting
+from os import walk
 
-working_dir = input("Enter a directory: ")
-print("Now, wait a bit...")
-Directory(working_dir).rename_dir()
+if argv[-1] == "--auto":
+    settings = load_setting()
+    for folder in settings["folders"]:
+        for itered_dir in walk(folder):
+            Directory(itered_dir[0]).rename_dir()
 
-print("Done!")
+else:
+    working_dir = input("Enter a directory: ")
+    print("Now, wait a bit...")
+    Directory(working_dir).rename_dir()
+
+    print("Done!")
